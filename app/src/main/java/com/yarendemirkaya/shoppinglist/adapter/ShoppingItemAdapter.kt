74434d38f9913f2e12201d29e70385f254b5bce8
yarendemirkaya <yarendmrky65@gmail.com>
2024-09-22 15:16:36 +0300

    Added dialog.

--- app/src/main/java/com/yarendemirkaya/shoppinglist/adapter/ShoppingItemAdapter.kt
+++ app/src/main/java/com/yarendemirkaya/shoppinglist/adapter/ShoppingItemAdapter.kt
@@ -5,24 +5,30 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.yarendemirkaya.shoppinglist.R
 import com.yarendemirkaya.shoppinglist.data.db.entities.ShoppingItem
 import com.yarendemirkaya.shoppinglist.ui.shoppinglist.ShoppingViewModel
 
+
 class ShoppingItemAdapter(
     var items: List<ShoppingItem>,
-    private val viewModel: ShoppingViewModel //adaptera viewModel gönderilmez. Bunu araştır.We need
+    private val viewModel: ShoppingViewModel//adaptera viewModel gönderilmez. Bunu araştır.We need
 // to delete the data from our adapter because we have to delete imageview so we need to call the delete
 // function from the viewmodel inside of adapter class.
 ) : RecyclerView.Adapter<ShoppingItemAdapter.ShoppingViewHolder>() {
 
+
     inner class ShoppingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoppingViewHolder {
+
+
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.shopping_item, parent, false)
         return ShoppingViewHolder(view)
+
     }
 
     override fun getItemCount(): Int {
@@ -31,6 +37,19 @@ class ShoppingItemAdapter(
 
     override fun onBindViewHolder(holder: ShoppingViewHolder, position: Int) {
         val curShoppingItem = items[position]
+        val builder: AlertDialog.Builder = AlertDialog.Builder(holder.itemView.context) //?
+        builder
+            .setMessage("Do you really want to delete this item")
+            .setTitle("Delete Item")
+            .setPositiveButton("YES") { dialog, which ->
+                    viewModel.delete(curShoppingItem)
+
+            }
+            .setNegativeButton("NO") { dialog, which ->
+                dialog.dismiss()
+            }
+
+        val deleteItemDialog: AlertDialog = builder.create()
 
 
         holder.itemView.findViewById<TextView>(R.id.tvName).text = curShoppingItem.name
@@ -50,7 +69,11 @@ class ShoppingItemAdapter(
         holder.itemView.findViewById<ImageView>(R.id.ivMinus).setOnClickListener {
             if (curShoppingItem.amount > 0) {
                 curShoppingItem.amount--
-                viewModel.upsert(curShoppingItem)
+                if (curShoppingItem.amount == 0) {
+                    deleteItemDialog.show()
+                } else {
+                    viewModel.upsert(curShoppingItem)
+                }
             }
         }
     }
--- app/src/main/java/com/yarendemirkaya/shoppinglist/ui/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/yarendemirkaya/shoppinglist/ui/shoppinglist/MainActivity.kt
@@ -14,6 +14,7 @@ import com.yarendemirkaya.shoppinglist.data.db.entities.ShoppingItem
 
 class MainActivity : AppCompatActivity() {
 
+
     private val viewModel: ShoppingViewModel by viewModels<ShoppingViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +25,7 @@ class MainActivity : AppCompatActivity() {
          //val database = ShoppingDatabase(this) //bunlar bad practice. DI olayına tamamen ters. tightly coupled yapmıs olursun.
          //val repository = ShoppingRepository(database)
 
-        val adapter = ShoppingItemAdapter(listOf(), viewModel)
+        val adapter = ShoppingItemAdapter(listOf(),viewModel)
         val rvShoppingItems = findViewById<RecyclerView>(R.id.rvShoppingItems)
 
         rvShoppingItems.layoutManager = LinearLayoutManager(this)
